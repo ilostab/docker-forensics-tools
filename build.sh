6bccd808a48643ba0261354dbd0758d4c215444b
ilostab <92776796+ilostab@users.noreply.github.com>
2024-09-11 14:37:03 +0200

    Exclude dev_*

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-find . -maxdepth 1 -type d ! -name "thor" -exec sh -c 'docker build -t "{}" "{}"' \;
+find . -maxdepth 1 -type d ! -name "thor" ! -name "dev_*" -exec sh -c 'docker build -t "{}" "{}"' \;
 
-# EXCLUDED Thor by default
+# EXCLUDED FOLDERS: Thor, dev_* 
